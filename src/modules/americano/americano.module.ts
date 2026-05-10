abb65e60bf3fb61ce75acb57fd5ef5c36e826782
herto2007-prog <herto2007@gmail.com>
2026-05-10 13:43:00 -0300

    fix: add TournamentsModule to AmericanoModule imports

--- src/modules/americano/americano.module.ts
+++ src/modules/americano/americano.module.ts
@@ -3,9 +3,10 @@ import { AmericanoService } from './americano.service';
 import { AmericanoController } from './americano.controller';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { AuthModule } from '../auth/auth.module';
+import { TournamentsModule } from '../tournaments/tournaments.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [PrismaModule, AuthModule, TournamentsModule],
   controllers: [AmericanoController],
   providers: [AmericanoService],
   exports: [AmericanoService],
